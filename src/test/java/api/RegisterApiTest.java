02b9dd713bf7d6779cc85f4fa9c8de18c7534cbd
mtyburski1 <98106499+mtyburski1@users.noreply.github.com>
2022-03-09 09:57:14 +0100

    Api

---
+++ src/test/java/api/RegisterApiTest.java
@@ -0,0 +1,58 @@
+package api;
+
+import models.User;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import providers.UserFactory;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class RegisterApiTest {
+    @Test
+    public void shouldRegisterByApi() {
+        baseURI = "http://madryt.app.javagirl.pl:22322/api";
+
+        User user = UserFactory.getRandomUser();
+
+        JSONObject request = new JSONObject();
+
+        request.put("firstName", user.getFirstName());
+        request.put("lastName", user.getLastName());
+        request.put("password", user.getPassword());
+        request.put("repeatedPassword", user.getPassword());
+        request.put("email", user.getEmail());
+        request.put("username", user.getLogin());
+
+        given()
+                .contentType("application/json\r\n")
+                .body(request.toJSONString())
+                .when()
+                .post("/user/register")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void shouldRegistrerWithThreeFields() {
+        baseURI = "http://madryt.app.javagirl.pl:22322/api";
+
+        User user = UserFactory.getRandomUser();
+
+        JSONObject request = new JSONObject();
+
+        request.put("password", user.getPassword());
+        request.put("repeatedPassword", user.getPassword());
+        request.put("username", user.getLogin());
+
+        given()
+                .contentType("application/json\r\n")
+                .body(request.toJSONString())
+                .when()
+                .post("/user/register")
+                .then()
+                .statusCode(200);
+
+    }
+}
+
